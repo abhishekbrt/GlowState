e7c40eb46e6d46f6269c70eaf9deb7c252dacc2b
abhishekbrt <abhishekbharti.yz@gmail.com>
2026-01-11 16:04:29 +0530

    chore: Update analysis_options and fix widget test after auth removal

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,11 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:glowstate/app.dart';
 
 void main() {
-  testWidgets('GlowState app loads login screen', (WidgetTester tester) async {
+  testWidgets('GlowState app loads home screen', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: GlowStateApp()));
 
     await tester.pumpAndSettle();
 
-    expect(find.text('Welcome back'), findsOneWidget);
+    // App should load directly to home screen (no login required)
+    expect(find.text('GlowState'), findsOneWidget);
   });
 }
